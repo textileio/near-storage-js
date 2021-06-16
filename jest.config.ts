c34c20ee1bc572dc011fdd3035b7647cb2739d6b
Carson Farmer <carson.farmer@gmail.com>
2021-06-16 14:54:41 -0700

    test: enables tests + prelim jws tests
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

---
+++ jest.config.ts
@@ -0,0 +1,35 @@
+export default {
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+
+  // Automatically reset mock state between every test
+  resetMocks: false,
+
+  // Automatically restore mock state between every test
+  restoreMocks: false,
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+};
---
+++ src/tests/jws.test.ts
@@ -0,0 +1,78 @@
+import { InMemorySigner } from "near-api-js";
+import type { PublicKey } from "near-api-js/src/utils/key_pair";
+import { InMemoryKeyStore } from "near-api-js/lib/key_stores/in_memory_key_store";
+import { decodeURLSafe, encodeURLSafe } from "@stablelib/base64";
+import { encode } from "bs58";
+
+import { jws } from "../jws";
+
+const decoder = new TextDecoder();
+const signer = new InMemorySigner(new InMemoryKeyStore());
+const accountId = "account.testnet";
+const networkId = "network.id";
+const aud = "broker.testnet";
+
+let publicKey: PublicKey;
+
+beforeAll(async () => {
+  publicKey = await signer.createKey(accountId, networkId);
+});
+
+test("create basic jws token", async () => {
+  const token = await jws(signer, {
+    accountId,
+    networkId,
+    aud,
+  });
+  expect(typeof token).toEqual("string");
+  expect(token).toContain(".");
+});
+
+test("jws has correct header", async () => {
+  const jwk = {
+    kty: "OKP",
+    crv: "Ed25519",
+    x: encodeURLSafe(publicKey.data),
+    use: "sig",
+  };
+  const token = await jws(signer, {
+    accountId,
+    networkId,
+    aud,
+  });
+  const [h] = token.split(".");
+  const header = JSON.parse(decoder.decode(decodeURLSafe(h)));
+  expect(header).toHaveProperty("alg", "EdDSASha256");
+  expect(header).toHaveProperty("typ", "JWT");
+  expect(header).toHaveProperty("jwk", jwk);
+});
+
+test("jws has correct payload", async () => {
+  const buffer = new Uint8Array([0xed, 0x01, ...publicKey.data]);
+  const id = `z${encode(buffer)}`;
+  const token = await jws(signer, {
+    accountId,
+    networkId,
+    aud,
+  });
+  const [, p] = token.split(".");
+  const payload = JSON.parse(decoder.decode(decodeURLSafe(p)));
+  expect(payload).toHaveProperty("iss", accountId);
+  expect(payload).toHaveProperty("sub", `did:key:${id}`);
+  expect(payload).toHaveProperty("aud", aud);
+  const { exp, iat } = payload;
+  expect(exp).toBeGreaterThan(iat);
+});
+
+test("jws supports overriding defaults", async () => {
+  const yesterday = ((d) => new Date(d.setDate(d.getDate() - 1)))(new Date());
+  const token = await jws(signer, {
+    accountId,
+    networkId,
+    aud,
+    exp: yesterday.valueOf(),
+  });
+  const [, p] = token.split(".");
+  const payload = JSON.parse(decoder.decode(decodeURLSafe(p)));
+  expect(payload).toHaveProperty("exp", yesterday.valueOf());
+});
